8fdfc3136198f4008f4ee4f083414e869309f811
christian <cktshukudu@gmail.com>
2021-08-17 16:10:18 +0200

    update all the changes

---
+++ shop.js
@@ -0,0 +1,41 @@
+var smallAddBtn = document.querySelector(".smallAdd");
+var mediumAddBtn = document.querySelector(".mediumAdd");
+var largeAddBtn = document.querySelector(".largeAdd");
+//reference buttons in the
+var smallBtn = document.querySelector(".smallBtn");
+var mediumBtn = document.querySelector(".mediumBtn");
+var largeBtn = document.querySelector(".largeBtn");
+
+var smallRemoveBtn = document.querySelector(".smallRemove");
+var mediumRemoveBtn = document.querySelector(".mediumRemove");
+var largeRemoveBtn = document.querySelector(".largeRemove");
+
+var smallQuantity = document.querySelector(".smallQuantity");
+var mediumQuantity = document.querySelector(".mediumQuantity");
+var largeQuantity = document.querySelector(".largeQuantity");
+
+var smallPizzaTotal = document.querySelector(".smallPizzaTotal");
+var medPizzaTotal = document.querySelector(".medPizzaTotal");
+var largePizzaTotal = document.querySelector(".largePizzaTotal");
+var totalCost = document.querySelector(".totalCost");
+
+var checkOut = document.querySelector(".checkOut");
+var payAmount = document.querySelector(".payAmount");
+var paymentBtn = document.querySelector(".paymentBtn");
+var payOut = document.querySelector(".payOut");
+var message = document.querySelector(".message");
+
+
+var smallPizzaQty = 0;
+var medPizzaQty = 0;
+var largePizzaQty = 0;
+var totalCosts = 0;
+
+var small = 30;
+var medium = 60;
+var large = 90;
+
+
+
+
+
